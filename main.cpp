0b54402c7d64ba7e249424a3c0bdef9224bd798c
AlisovTimofey <timofeyalisov@gmail.com>
2026-03-19 15:00:48 +0300

    smth

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "top-it-vector.hpp"
 #include <iostream>
+#include <cassert>
 
 bool testDefaultVector()
 {
@@ -14,12 +15,35 @@ bool testVectorWithValue()
   return !v.isEmpty();
 }
 
+bool testElemtAccess()
+{
+  top::it::Vector<int> v;
+  v.pushBack(1);
+  v.pushBack(2);
+  return v[0] == 1 && v[1] == 2;
+}
+
+bool testCopyConstructor()
+{
+  topit::Vector<int> v;
+  v.pushBack(1);
+  topit::Vector<int> copy = v;
+  bool isAllEqual = v.getSize() == copy.getSize();
+  for (size_t i = 0; isAllEqual && i < v.getSize(); ++i)
+  {
+    isAllEqual = isAllEqual && (v[i] == copy[i]);
+  }
+  return isAllEqual;
+}
+
 int main()
 {
   using test_t = bool (*)();
   using pair_t = std::pair<const char *, test_t>;
   pair_t tests[] = {{"Default vector should be empty", testDefaultVector},
-                    {"Vector with any value is not empty", testVectorWithValue}};
+                    {"Vector with any value is not empty", testVectorWithValue},
+                    {"Element access should work", testElemtAccess},
+                    {"Copy constructor should work", testCopyConstructor}};
   const size_t count = sizeof(tests) / sizeof(pair_t);
   std::cout << std::boolalpha;
   bool pass = true;
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -12,19 +12,51 @@ namespace topit
     ~Vector();
     Vector<T> &operator=(const Vector<T> &) = delete;
 
+    void swap(Vector<T> &rhs) noexcept;
+
     bool isEmpty() const noexcept;
     size_t getSize() const noexcept;
     size_t getCapacity() const noexcept;
 
+    T &operator[](size_t id) noexcept;
+    const T &operator[](size_t id) const noexcept;
+
     void pushBack(const T &);
     void popBack();
+    void pushFront(const T &);
 
   private:
     T *data_;
     size_t size_, capacity_;
+
+    explicit Vector(size_t k);
   };
 }
 
+template <class T>
+void topit::Vector<T>::pushFront(const T &val)
+{
+  Vector<T> copy(val.getSize() + 1);
+  copy[0] = val;
+  for (size_t i = 1; i < getSize(); ++i)
+  {
+    copy[i] = (*this)[i - 1];
+  }
+  swap(copy);
+}
+
+template <class T>
+void topit::Vector<T>::swap(Vector<T> &rhs) noexcept
+{
+  std::swap(rhs.data_, data_);
+  std::swap(rhs.size_, size_);
+  std::swap(rhs.capacity_, capacity_);
+}
+
+template <class T>
+topit::Vector<T>::Vector(size_t k) : data_(new T[k]), size_(k), capacity_(k)
+{
+}
 template <class T>
 bool topit::Vector<T>::isEmpty() const noexcept
 {
@@ -40,6 +72,41 @@ template <class T>
 topit::Vector<T>::~Vector()
 {
   delete[] data_;
+}
+
+template <class T>
+topit::Vector<T> &topit::Vector<T>::operator=(const Vector<T> &rhs)
+{
+  Vector<T> copy(rhs);
+  swap(copy);
+
+  return *this;
+}
+
+template <class T>
+topit::Vector<T>::Vector(const Vector<T> &rhs) : Vector(rhs.getSize())
+{
+  for (size_t i = 0; i < getSize(); ++i)
+  {
+    data_[i] = rhs[i];
+  }
+}
+template <class T>
+size_t topit::Vector<T>::getSize() const noexcept
+{
+  return size_;
+}
+template <class T>
+T &topit::Vector<T>::operator[](size_t id) noexcept
+{
+  assert(id < getSize());
+  return data_[0];
+}
+template <class T>
+const T &topit::Vector<T>::operator[](size_t id) const noexcept
+{
+  assert(id < getSize());
+  return data_[0];
 }
 
 template <class T>
